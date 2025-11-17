994ecd21db9d82ad611a932eb0d0f80f4efbf44a
Oscar <orss.dev@gmail.com>
2025-11-17 13:45:33 -0500

    collected index urls

---
+++ allmovie_scrapper/spiders/allmovie_spider.py
@@ -0,0 +1,36 @@
+import scrapy
+
+class AllMovie_Scrapper(scrapy.Spider):
+    name = 'allmovie'
+
+    
+    async def start(self):
+        urls = [
+            "https://www.allmovie.com/genre/action-adventure-ag100",
+            "https://www.allmovie.com/genre/animation-ag102",
+            "https://www.allmovie.com/genre/anime-ag103",
+            "https://www.allmovie.com/genre/avant-garde-experimental-ag104",
+            "https://www.allmovie.com/genre/biography-ag105",
+            "https://www.allmovie.com/genre/childrens-ag106",
+            "https://www.allmovie.com/genre/comedy-ag107",
+            "https://www.allmovie.com/genre/comedy-drama-ag108",
+            "https://www.allmovie.com/genre/crime-ag109",
+            "https://www.allmovie.com/genre/documentary-ag110",
+            "https://www.allmovie.com/genre/drama-ag111",
+            "https://www.allmovie.com/genre/epic-ag112",
+            "https://www.allmovie.com/genre/family-ag113",
+            "https://www.allmovie.com/genre/fantasy-ag114",
+            "https://www.allmovie.com/genre/history-ag115",
+            "https://www.allmovie.com/genre/horror-ag116",
+            "https://www.allmovie.com/genre/mature-ag101",
+            "https://www.allmovie.com/genre/music-ag117",
+            "https://www.allmovie.com/genre/mystery-suspense-ag118",
+            "https://www.allmovie.com/genre/romance-ag120",
+            "https://www.allmovie.com/genre/science-fiction-ag121",
+            "https://www.allmovie.com/genre/silent-film-ag122",
+            "https://www.allmovie.com/genre/sports-ag123",
+            "https://www.allmovie.com/genre/spy-film-ag124",
+            "https://www.allmovie.com/genre/thriller-ag125",
+            "https://www.allmovie.com/genre/war-ag126",
+            "https://www.allmovie.com/genre/western-ag127",
+        ]
---
+++ collect_urls.py
@@ -0,0 +1,27 @@
+# For collecting the indexes urls to scrape movie data
+
+import requests
+import os
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+from protego import Protego
+
+load_dotenv()
+
+user_agent = os.getenv('USER_AGENT')
+HEADERS = {'user-agent': user_agent}
+url = 'https://www.allmovie.com'
+response = requests.get(url + '/robots.txt', headers=HEADERS)
+rp = Protego.parse(response.text)
+crawl_delay = rp.crawl_delay(user_agent) or 0
+
+if rp.can_fetch('https://www.allmovie.com/genres', user_agent):
+     response = requests.get('https://www.allmovie.com/genres', headers=HEADERS)
+     soup = BeautifulSoup(response.text, 'html.parser')
+     content = soup.select('.content a[href^="https"]')
+     # Unix pipe into file `python collect_urls.py > urls.txt`
+     print('[')
+     for a in content:
+          href = a.get('href')
+          print(f'\t"{href}",')
+     print(']')
