c1e4b47e366f5b43e088e271b5a011e957f4409c
Oscar <orss.dev@gmail.com>
2025-11-18 14:31:20 -0500

    mpaa and seen update

--- allmovie_scrapper/spiders/allmovie_spider.py
+++ allmovie_scrapper/spiders/allmovie_spider.py
@@ -6,45 +6,47 @@ import os
 class AllMovie_Scrapper(scrapy.Spider):
     name = 'allmovie'
     seen_url = set()
+    mpaa = set(['G', 'PG', 'PG-13', 'R', 'NC-17', 'NR', 'TV-Y', 'TV-Y7', 'TV-G', 'TV-PG', 'TV-14', 'TV-MA'])
 
     async def start(self):
-        self.seen_url = set()
         try:
             path = os.path.join('..', '..', 'data.jsonl')
             with open(path, 'r') as lines:
                 for line in lines:
                     data = json.loads(line)
-                    self.seen_url.add(data['url'])
+                    url = data.get('url')
+                    if url:
+                        self.seen_url.add()
         except FileNotFoundError:
             pass
             
         urls = [
             "https://www.allmovie.com/genre/action-adventure-ag100",
-            "https://www.allmovie.com/genre/animation-ag102",
-            "https://www.allmovie.com/genre/anime-ag103",
-            "https://www.allmovie.com/genre/avant-garde-experimental-ag104",
-            "https://www.allmovie.com/genre/biography-ag105",
-            "https://www.allmovie.com/genre/childrens-ag106",
-            "https://www.allmovie.com/genre/comedy-ag107",
-            "https://www.allmovie.com/genre/comedy-drama-ag108",
-            "https://www.allmovie.com/genre/crime-ag109",
-            "https://www.allmovie.com/genre/documentary-ag110",
-            "https://www.allmovie.com/genre/drama-ag111",
-            "https://www.allmovie.com/genre/epic-ag112",
-            "https://www.allmovie.com/genre/family-ag113",
-            "https://www.allmovie.com/genre/fantasy-ag114",
-            "https://www.allmovie.com/genre/history-ag115",
-            "https://www.allmovie.com/genre/horror-ag116",
-            "https://www.allmovie.com/genre/music-ag117",
-            "https://www.allmovie.com/genre/mystery-suspense-ag118",
-            "https://www.allmovie.com/genre/romance-ag120",
-            "https://www.allmovie.com/genre/science-fiction-ag121",
-            "https://www.allmovie.com/genre/silent-film-ag122",
-            "https://www.allmovie.com/genre/sports-ag123",
-            "https://www.allmovie.com/genre/spy-film-ag124",
-            "https://www.allmovie.com/genre/thriller-ag125",
-            "https://www.allmovie.com/genre/war-ag126",
-            "https://www.allmovie.com/genre/western-ag127",
+            # "https://www.allmovie.com/genre/animation-ag102",
+            # "https://www.allmovie.com/genre/anime-ag103",
+            # "https://www.allmovie.com/genre/avant-garde-experimental-ag104",
+            # "https://www.allmovie.com/genre/biography-ag105",
+            # "https://www.allmovie.com/genre/childrens-ag106",
+            # "https://www.allmovie.com/genre/comedy-ag107",
+            # "https://www.allmovie.com/genre/comedy-drama-ag108",
+            # "https://www.allmovie.com/genre/crime-ag109",
+            # "https://www.allmovie.com/genre/documentary-ag110",
+            # "https://www.allmovie.com/genre/drama-ag111",
+            # "https://www.allmovie.com/genre/epic-ag112",
+            # "https://www.allmovie.com/genre/family-ag113",
+            # "https://www.allmovie.com/genre/fantasy-ag114",
+            # "https://www.allmovie.com/genre/history-ag115",
+            # "https://www.allmovie.com/genre/horror-ag116",
+            # "https://www.allmovie.com/genre/music-ag117",
+            # "https://www.allmovie.com/genre/mystery-suspense-ag118",
+            # "https://www.allmovie.com/genre/romance-ag120",
+            # "https://www.allmovie.com/genre/science-fiction-ag121",
+            # "https://www.allmovie.com/genre/silent-film-ag122",
+            # "https://www.allmovie.com/genre/sports-ag123",
+            # "https://www.allmovie.com/genre/spy-film-ag124",
+            # "https://www.allmovie.com/genre/thriller-ag125",
+            # "https://www.allmovie.com/genre/war-ag126",
+            # "https://www.allmovie.com/genre/western-ag127",
         ]
 
         for url in urls:
@@ -92,10 +94,10 @@ class AllMovie_Scrapper(scrapy.Spider):
             elif i == 1:
                 runtime = int(detail.split()[0])
             elif i == 2:
-                if len(detail) > 4:
-                    country = detail
-                else:
+                if detail in self.mpaa:
                     mpaa_rating = detail
+                else:
+                    country = detail
             else:
                 mpaa_rating = detail
         budget = response.css('.budget .charactList::text').get()
