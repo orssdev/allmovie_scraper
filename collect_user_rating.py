27eb5e3ad14a10576d2dc9704f54afd1630b4525
Oscar <orss.dev@gmail.com>
2025-11-20 01:54:24 -0500

    following robots.txt

--- collect_user_rating.py
+++ collect_user_rating.py
@@ -4,6 +4,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import os
+import requests
+from dotenv import load_dotenv
+from protego import Protego
+
+load_dotenv()
+
+user_agent = os.getenv('USER_AGENT')
+HEADERS = {'user-agent': user_agent}
+url = 'https://www.allmovie.com'
+response = requests.get(url + '/robots.txt', headers=HEADERS)
+rp = Protego.parse(response.text)
 
 
 options = Options()
@@ -40,26 +52,27 @@ try:
             movie_id = data.get('id')
             url = data.get('url')
             if movie_id not in seen_user:
-                driver.get(url)
-                flag = False
-                try:
-                    user_count = int(WebDriverWait(driver, 10).until(
-                        EC.presence_of_element_located((By.CSS_SELECTOR, ".average-user-rating-count"))
-                    ).text)
-                    user_rating_elem = driver.find_element(By.CSS_SELECTOR, '.average-user-rating')
-                    user_rating = int(user_rating_elem.get_attribute('class').split()[1].split('-')[2])
-                    flag = True
-                except IndexError:
-                    print(json.dumps({'url': url, 'id': movie_id, 'error': 'index'}))
-                except ValueError:
-                    print(json.dumps({'url': url, 'id': movie_id, 'error': 'int cast'}))
-                except:
-                    user_count = 0
-                    user_rating = None
-                    flag = True
+                if rp.can_fetch(url, user_agent):
+                    driver.get(url)
+                    flag = False
+                    try:
+                        user_count = int(WebDriverWait(driver, 10).until(
+                            EC.presence_of_element_located((By.CSS_SELECTOR, ".average-user-rating-count"))
+                        ).text)
+                        user_rating_elem = driver.find_element(By.CSS_SELECTOR, '.average-user-rating')
+                        user_rating = int(user_rating_elem.get_attribute('class').split()[1].split('-')[2])
+                        flag = True
+                    except IndexError:
+                        print(json.dumps({'url': url, 'id': movie_id, 'error': 'index'}))
+                    except ValueError:
+                        print(json.dumps({'url': url, 'id': movie_id, 'error': 'int cast'}))
+                    except:
+                        user_count = 0
+                        user_rating = None
+                        flag = True
 
-                if flag:
-                    file.write(json.dumps({'id': movie_id, 'user_count': user_count, 'user_rating': user_rating}) + '\n')
+                    if flag:
+                        file.write(json.dumps({'id': movie_id, 'user_count': user_count, 'user_rating': user_rating}) + '\n')
         driver.quit()
 except FileNotFoundError:
     print('data.jsonl not found')
