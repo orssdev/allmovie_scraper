179cc4896926ce376e8d5ca529d849292b3ed09d
Oscar <orss.dev@gmail.com>
2025-11-20 17:35:46 -0500

    root

--- combine.py
+++ combine.py
@@ -1,14 +1,13 @@
 import pandas as pd
 import os
 
-data = pd.read_json(os.path.join('..', 'data.jsonl'), lines=True).sort_values(by='id').reset_index(drop=True)
-users = pd.read_json(os.path.join('..', 'users.jsonl'), lines=True).sort_values(by='id').reset_index(drop=True)
+data = pd.read_json('data.jsonl', lines=True).sort_values(by='id').reset_index(drop=True)
+users = pd.read_json('users.jsonl', lines=True).sort_values(by='id').reset_index(drop=True)
 
 combined = data.join(users.set_index('id'), on='id')
 filtered = combined[combined['user_count'].notna()]
 result = filtered.drop(columns=['url', 'poster_url', 'country', 'themes', 'subgenres']).reset_index(drop=True)
 result['directors'] = result['directors'].apply(lambda x: [d['name'] for d in x] if isinstance(x, list) else [])
 
-# print(result.iloc[:, :3].head(20))
 print(result)
 result.to_csv('data.csv', index=False) 
