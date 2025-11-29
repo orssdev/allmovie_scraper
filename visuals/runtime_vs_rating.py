22f30a39a52c5c31fa83b38a04015db1557e5c42
Oscar <orss.dev@gmail.com>
2025-11-29 15:28:13 -0500

    hex heat graph

--- visuals/runtime_vs_rating.py
+++ visuals/runtime_vs_rating.py
@@ -2,11 +2,11 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
+plt.style.use('dark_background')
 df = pd.read_csv(os.path.join('..', 'data', 'data.csv'))
-
-plt.figure()
-plt.scatter(df['runtime'], df['user_rating'])
+plt.hexbin(df['runtime'], df['user_rating'], gridsize=30, cmap='Reds')
 plt.xlabel("Runtime (minutes)")
 plt.ylabel("User Rating")
-plt.title("Duration vs. Rating")
+plt.title("Duration vs. Rating (Hexbin)")
+plt.colorbar(label='Count')
 plt.show()
