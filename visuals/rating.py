4e2d3a2dc7e9d062cd59868fd268d9e9898c2b79
Oscar <orss.dev@gmail.com>
2025-11-29 16:10:43 -0500

    red

--- visuals/rating.py
+++ visuals/rating.py
@@ -1,11 +1,12 @@
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
 df = pd.read_csv(os.path.join('..', 'data', 'data.csv'))
-
+plt.style.use('dark_background')
 plt.figure()
-plt.hist(df['user_rating'].dropna(), bins=20)
+plt.hist(df['user_rating'].dropna(), bins=np.arange(0.5, 11.5, 1), color='red', edgecolor='darkred', linewidth=1.2 )
 plt.xlabel("User Rating")
 plt.ylabel("Number of Movies")
 plt.title("Rating Distribution")
